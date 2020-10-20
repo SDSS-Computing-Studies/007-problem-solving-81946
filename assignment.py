65ad3ba60e436ea80996c51e427bf5e23894793d
81946 <71406891+81946@users.noreply.github.com>
2020-10-20 13:02:24 -0700

    Submit

--- assignment.py
+++ assignment.py
@@ -2,6 +2,7 @@
 
 import pyautogui as p 
 import time as t
+
 p.mouseInfo()
 
 #1-Set up auto clicker in one spot to click.
@@ -12,56 +13,100 @@ p.mouseInfo()
 #Game- https://particle-clicker.web.cern.ch/
 
 
-point= True
-times=[]
-events = []
+
+
+def blue(x):
+    finger=True
+    times=[]
+    events= []
+    x=1
+    if x==1:
+        p.click(1590,170)
+        while finger:
+            JTN= p.locateCenterOnScreen('Person icon.png')
+            p.click(JTN)
+            print(".", end="")
+
+
+            curTime = t.time()
 
 
 
-
-
-while point:
-    p.click(1200,387)
-    print(".", end="")
-
-
-    curTime = t.time()
+            times.append(curTime + 120)
+            events.append('120 seconds')
 
 
 
-    times.append(curTime + 10)
-    events.append('10 seconds')
+            numEvents = len(events)
+            index = 0
 
+            curTime = t.time()
 
-
-    numEvents = len(events)
-    index = 0
-
-    curTime = t.time()
-
-    for i in times:
+            for i in times:
         
-        if curTime > i:
-            print(events[index])
+                if curTime > i:
+                    print(events[index])
             
-            times.pop(index)
-            events.pop(index)
-            point= False
+                    times.pop(index)
+                    events.pop(index)
+                    finger= False
+
+
+
+                    click()
+
+    
+    
+    
+    
+
+
+def click():
+    point= True
+    times=[]
+    events = []
+    x=0
+    while point:
+        p.click(1200,387)
+        print(".", end="")
+
+
+        curTime = t.time()
+
+
+
+        times.append(curTime + 15)
+        events.append('15 seconds')
+
+
+
+        numEvents = len(events)
+        index = 0
+
+        curTime = t.time()
+
+        for i in times:
+        
+            if curTime > i:
+                print(events[index])
             
-            blue()?????
+                times.pop(index)
+                events.pop(index)
+                point= False
+            
+                blue(x)
 
 
 
 
 
 
+click()
 
 
-def blue():
-    locateOnScreen(p.getpixel(40,96,144))
-    
-    
-    
+
+
+
     
 
 
